0843b14d288803a5fd5623b5a03e0a5756844798
Antoine-NVE <antoinenavette@gmail.com>
2024-08-17 11:30:40 +0200

    Épisode 12 : Upload d'Images et Redimensionnement

--- app/src/Controller/Profile/PostsController.php
+++ app/src/Controller/Profile/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Profile;
 use App\Entity\Posts;
 use App\Form\AddPostFormType;
 use App\Repository\UsersRepository;
+use App\Service\PictureService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,7 +29,8 @@ class PostsController extends AbstractController
         Request $request,
         SluggerInterface $slugger,
         EntityManagerInterface $em,
-        UsersRepository $usersRepository
+        UsersRepository $usersRepository,
+        PictureService $pictureService
     ): Response {
         $post = new Posts();
 
@@ -39,9 +41,13 @@ class PostsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $post->setSlug(strtolower($slugger->slug($post->getTitle())));
 
-            $post->setUsers($usersRepository->find(1));
+            $post->setUsers($this->getUser());
 
-            $post->setFeaturedImage('default.webp');
+            $featuredImage = $form->get('featuredImage')->getData();
+
+            $image = $pictureService->square($featuredImage, 'articles', 300);
+
+            $post->setFeaturedImage($image);
 
             $em->persist($post);
             $em->flush();
